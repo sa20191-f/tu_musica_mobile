574f3fa0cb06fc3c1995080cc5cc1e877430a21c
Federico G <federgolo@hotmail.com>
2019-06-03 00:21:37 -0500

    Prettify initial screen

--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -1,30 +1,78 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, Button} from 'react-native';
+import { View, ScrollView, StyleSheet, Text, Button, ImageBackground } from 'react-native';
 import { withNavigation } from 'react-navigation';
 
 class InitialScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+  state = {
+    response: '',
+  }
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Initial</Text>
-        <Button
-          onPress={this._navigate}
-          title="Descubre mas de nosotros"
-        />
-          
-      </ScrollView>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('../assets/images/bg-1.jpg')}
+          style={styles.imageContainer}
+        >
+          <View style={styles.artistContainer}>
+            <View style={styles.artistDetailWrapper}>
+              <Text style={styles.slogan}>LA MEJOR MÚSICA</Text>
+              <Text style={styles.title}>TU MUSICA</Text>
+            </View>
+          </View>
+
+          <Button
+            onPress={this._navigate}
+            title="Go to Login"
+          />
+        </ImageBackground>
+      </View>
     );
   }
   _navigate = () => {
-    this.props.navigation.navigate('Main');
+    this.props.navigation.navigate('LoginScreen');
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+  },
+  imageContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  artistContainer: {
+    flex: 1,
+    paddingTop: 24,
+    flexDirection: 'row',
+    paddingLeft: 20,
+    alignItems: 'center',
+    paddingRight: 20,
+  },
+  artistDetailWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#fff',
+    letterSpacing: 10,
+    textAlign: 'center',
+  },
+  slogan: {
+    fontSize: 22,
+    color: '#fff',
+    letterSpacing: 5,
+    textTransform: 'capitalize',
+    marginBottom: 20,
+    textAlign: 'center',
   },
 });
 
