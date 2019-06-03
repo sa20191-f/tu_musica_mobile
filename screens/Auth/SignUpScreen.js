ab823e20dd667edfec13b13e3dd6f9319b08909b
Federico G <federgolo@hotmail.com>
2019-06-03 11:33:19 -0500

    Create forms on signup and login screens

--- screens/Auth/SignUpScreen.js
+++ screens/Auth/SignUpScreen.js
@@ -1,20 +1,81 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Button, Text } from 'react-native';
+
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  username: t.String,
+  email: t.String,
+  password: t.String,
+  // terms: t.Boolean
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
 
 export default class SignUpScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Crea una cuenta',
+  };
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+  
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>SignUp</Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.textContainer}>Únete a esta gran familia</Text>
+        <Form 
+          
+          ref={c => this._form = c}
+          type={User} 
+        />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  textContainer: {
+    // flex: 1,
+    fontSize: 22,
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#f00',
+  },
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
